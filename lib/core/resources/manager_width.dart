686e7bd31b60577b1e9e530529721fda15b8b1d1
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-02 19:16:49 +0300

    Add consts

--- lib/core/resources/manager_width.dart
+++ lib/core/resources/manager_width.dart
@@ -5,11 +5,15 @@ class ManagerWidth {
   static const double w8 = 8;
   static const double w10 = 10;
   static const double w12 = 12;
+  static const double w16 = 16;
+  static const double w20 = 20;
   static const double w30 = 30;
   static const double w50 = 50;
   static const double w40 = 40;
   static const double w56 = 56;
   static const double w60 = 60;
+  static const double w64 = 64;
+  static const double w80 = 80;
   static const double w100 = 100;
   static const double w200 = 200;
 }
