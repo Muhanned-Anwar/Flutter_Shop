98ff9ce984c65a84c1cd4d7bf0f1e56dd319fc8a
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-02 16:31:18 +0300

    Create base screen

---
+++ lib/features/out_boarding/presentation/view/out_boarding_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OutBoardingScreen extends StatefulWidget {
+  const OutBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OutBoardingScreen> createState() => _OutBoardingScreenState();
+}
+
+class _OutBoardingScreenState extends State<OutBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
