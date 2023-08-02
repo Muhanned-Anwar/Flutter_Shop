31ef99a91a743653348ff0cb124f682111986a32
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-02 15:59:30 +0300

    Create base screen

---
+++ lib/features/splash/presentation/view/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../../../core/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(
+        seconds: Constants.splashTime,
+      ),
+      () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
