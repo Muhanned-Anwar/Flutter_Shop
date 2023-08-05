dba1ab34cc99be279e64e37e2b7194eb0a81bb6d
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-05 16:24:49 +0300

    add string and constants

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   static const double appBarElevation = 0;
+  static const double elevationZero = 0;
   static const int splashTime = 3;
+  static const int baseButtonFirstSpacerFlex = 5;
+  static const int pageViewSliderDuration = 300;
 }
--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -13,4 +13,5 @@ class ManagerStrings {
   static const String outBoardingSubTitle4 =
       'Quia sed consequatur rem quia molestias nulla quia. Eos optio soluta asperiores in similique. Eius tenetur blanditiis ad cumque aspernatur earum nihil qui. Eveniet doloribus cum eum rerum facilis aut. Et quos dolores eos sed ex voluptatem dolorem est ut. Alias voluptatum dolorem occaecati cum atque in dolorum sequi qui.';
   static const String start = 'Start';
+  static const String next = 'Next';
 }
