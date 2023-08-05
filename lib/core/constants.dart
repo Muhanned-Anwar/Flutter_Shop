44850969ff5154c8449436477f918c6758eee08b
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-05 16:28:20 +0300

    Refactor BaseButton

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,7 +1,10 @@
 class Constants {
   static const double appBarElevation = 0;
   static const double elevationZero = 0;
+  static const double baseButtonElevation = 2;
   static const int splashTime = 3;
   static const int baseButtonFirstSpacerFlex = 5;
+  static const int baseButtonSecondSpacerFlex = 4;
   static const int pageViewSliderDuration = 300;
+  static const bool baseButtonVisibleIcon = false;
 }
