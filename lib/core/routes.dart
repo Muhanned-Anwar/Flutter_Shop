0c540f766380e577ee575998865071225c4ef1cb
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-14 21:09:30 +0300

    add routes

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -2,4 +2,7 @@ class Routes {
   static const String splashScreen = '/splash_screen';
   static const String outBoardingScreen = '/out_boarding_screen';
   static const String authenticationView = '/authenticationView';
+  static const String loginView = '/loginView';
+  static const String registerView = '/registerView';
+  static const String homeView = '/HomeView';
 }
