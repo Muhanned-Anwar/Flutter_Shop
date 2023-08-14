8deeec9fbdb6126d9e381453f73a66715f95d6af
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-14 21:09:07 +0300

    create HomeView

---
+++ lib/features/home/presentation/view/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Home View'),
+      ),
+    );
+  }
+}
