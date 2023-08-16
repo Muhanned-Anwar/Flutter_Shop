3fc124918fd800cfcfeb83a9f1a5007b1aa68139
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-16 16:31:31 +0300

    create AppSettingsSharedPreferences

---
+++ lib/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart
@@ -0,0 +1,25 @@
+import 'package:avatar_course2_5_shop/core/constants.dart';
+import 'package:avatar_course2_5_shop/core/extension/extensions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSettingsSharedPreferences {
+  static final _instance = AppSettingsSharedPreferences._internal();
+  late SharedPreferences _sharedPreferences;
+
+  AppSettingsSharedPreferences._internal();
+
+  factory AppSettingsSharedPreferences() {
+    return _instance;
+  }
+
+  Future<void> initPreferences() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future<void> saveViewedOutBoarding() async {
+    await _sharedPreferences.setBool(KeyConstants.outBoardingViewedKey, true);
+  }
+
+  bool get outBoardingViewed =>
+      _sharedPreferences.getBool(KeyConstants.outBoardingViewedKey).onNull();
+}
