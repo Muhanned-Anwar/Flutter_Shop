90b04ff4553afaee249f1d2994e08908d9ad0e47
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-16 15:51:49 +0300

    use onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'package:avatar_course2_5_shop/core/routes.dart';
-import 'package:avatar_course2_5_shop/features/auth/presentation/view/authentication_view.dart';
-import 'package:avatar_course2_5_shop/features/auth/presentation/view/login_view.dart';
-import 'package:avatar_course2_5_shop/features/auth/presentation/view/register_view.dart';
-import 'package:avatar_course2_5_shop/features/home/presentation/view/home_view.dart';
-import 'package:avatar_course2_5_shop/features/out_boarding/presentation/view/out_boarding_screen.dart';
-import 'package:avatar_course2_5_shop/features/splash/presentation/view/splash_screen.dart';
+import 'package:avatar_course2_5_shop/route/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,14 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.splashScreen,
-      routes: {
-        Routes.splashScreen: (context) => const SplashScreen(),
-        Routes.outBoardingScreen: (context) => const OutBoardingScreen(),
-        Routes.authenticationView: (context) => const AuthenticationView(),
-        Routes.loginView: (context) => const LoginView(),
-        Routes.registerView: (context) => const RegisterView(),
-        Routes.homeView: (context) => const HomeView(),
-      },
+      onGenerateRoute: RouteGenerator.getRoute,
     );
   }
 }
