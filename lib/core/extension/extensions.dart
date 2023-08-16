5045d4079d17d42c5fa0f0974c78ef4865415ffb
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-16 16:31:45 +0300

    create extensions.dart file

---
+++ lib/core/extension/extensions.dart
@@ -0,0 +1,30 @@
+extension NonNullInt on int? {
+  int onNull() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullString on String? {
+  String onNull() {
+    if (this == null) {
+      return '';
+    } else {
+      return this!;
+    }
+  }
+}
+
+
+extension NonNullBoolean on bool? {
+  bool onNull() {
+    if (this == null) {
+      return false;
+    } else {
+      return this!;
+    }
+  }
+}
