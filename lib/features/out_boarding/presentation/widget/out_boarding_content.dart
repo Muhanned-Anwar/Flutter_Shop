1098379011e252e37c5355d163a1eaaaa906da98
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-17 14:07:18 +0300

    edit OutBoardingContent

--- lib/features/out_boarding/presentation/widget/out_boarding_content.dart
+++ lib/features/out_boarding/presentation/widget/out_boarding_content.dart
@@ -8,14 +8,14 @@ import '../../../../core/resources/manager_strings.dart';
 
 class OutBoardingContent extends StatelessWidget {
   final String image;
-  final String title;
-  final String subTitle;
+  final String? title;
+  final String? subTitle;
 
   const OutBoardingContent({
     super.key,
     this.image = ManagerAssets.outBoarding1,
-    this.title = ManagerStrings.outBoardingTitle1,
-    this.subTitle = ManagerStrings.outBoardingSubTitle1,
+    this.title,
+    this.subTitle,
   });
 
   @override
@@ -32,7 +32,7 @@ class OutBoardingContent extends StatelessWidget {
             height: ManagerHeight.h90,
           ),
           Text(
-            title,
+            title ?? ManagerStrings.outBoardingTitle1,
             style: TextStyle(
               fontSize: ManagerFontSizes.s26,
               fontWeight: ManagerFontWeight.bold,
@@ -45,7 +45,7 @@ class OutBoardingContent extends StatelessWidget {
           SizedBox(
             height: ManagerHeight.h100,
             child: Text(
-              subTitle,
+              subTitle ?? ManagerStrings.outBoardingSubTitle1,
               style: TextStyle(
                 fontSize: ManagerFontSizes.s18,
                 fontWeight: ManagerFontWeight.regular,
