3a58a27da5bdf2d650ec6d2e786ab3786c8ca954
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-19 17:20:32 +0300

    put for fonts

--- lib/core/resources/manager_font_sizes.dart
+++ lib/core/resources/manager_font_sizes.dart
@@ -1,9 +1,11 @@
+import 'package:avatar_course2_5_shop/core/utils/size_util.dart';
+
 class ManagerFontSizes {
-  static const double s14 = 14;
-  static const double s16 = 16;
-  static const double s18 = 18;
-  static const double s20 = 20;
-  static const double s24 = 24;
-  static const double s26 = 26;
-  static const double s36 = 36;
+  static double s14 = SizeUtil.setFontSize(14);
+  static double s16 = SizeUtil.setFontSize(16);
+  static double s18 = SizeUtil.setFontSize(18);
+  static double s20 = SizeUtil.setFontSize(20);
+  static double s24 = SizeUtil.setFontSize(24);
+  static double s26 = SizeUtil.setFontSize(26);
+  static double s36 = SizeUtil.setFontSize(36);
 }
