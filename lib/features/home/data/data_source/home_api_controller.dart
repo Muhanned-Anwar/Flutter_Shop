1e5be01865d10994ab97a6ae2a5b9e25c2c2ce94
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-26 16:00:24 +0300

    create get base function and categories method

--- lib/features/home/data/data_source/home_api_controller.dart
+++ lib/features/home/data/data_source/home_api_controller.dart
@@ -2,12 +2,11 @@ import 'dart:convert';
 import 'package:avatar_course2_5_shop/core/constants.dart';
 import 'package:avatar_course2_5_shop/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart';
 import 'package:avatar_course2_5_shop/core/widgets/helpers.dart';
-import 'package:avatar_course2_5_shop/features/auth/data/mapper/login_mapper.dart';
-import 'package:avatar_course2_5_shop/features/auth/data/response/login_response.dart';
-import 'package:avatar_course2_5_shop/features/auth/presentation/model/login_model.dart';
+import 'package:avatar_course2_5_shop/features/home/data/mapper/category_mapper.dart';
+import 'package:avatar_course2_5_shop/features/home/data/response/category_response.dart';
+import 'package:avatar_course2_5_shop/features/home/presentation/model/category_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 
 class HomeApiController with Helpers {
   AppSettingsSharedPreferences appSettingsSharedPreferences =
@@ -23,5 +22,41 @@ class HomeApiController with Helpers {
       body: body,
       headers: header,
     );
+  }
+
+  Future get({
+    required String endPoint,
+    required Map<String, String> header,
+  }) async {
+    return await http.get(
+      Uri.parse(endPoint),
+      headers: header,
+    );
+  }
+
+  Future<List<CategoryModel>> categories({required BuildContext context}) async {
+    http.Response response = await get(
+      endPoint: ApiRequest.categories,
+      header: {},
+    );
+
+    var json = jsonDecode(response.body);
+
+    List<dynamic> data = json['data'] ?? [];
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      return data.map((category) {
+        CategoryResponse categoryResponse = CategoryResponse.fromJson(category);
+        CategoryModel categoryModel = categoryResponse.toDomain();
+        return categoryModel;
+      }).toList();
+    }
+
+    showSnackBar(
+      context: context,
+      message: 'Something Went Error',
+      error: true,
+    );
+    return [];
   }
 }
