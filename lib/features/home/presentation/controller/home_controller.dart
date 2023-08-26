ca8c94783d718670c88e3b4487710040da546189
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-26 16:00:36 +0300

    read categories

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -1,6 +1,19 @@
+import 'package:avatar_course2_5_shop/features/home/data/data_source/home_api_controller.dart';
 import 'package:get/get.dart';
 import '../model/category_model.dart';
 
 class HomeController extends GetxController {
   List<CategoryModel> categories = [];
+  HomeApiController homeApiController = HomeApiController();
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    readCategories();
+  }
+
+  readCategories() async {
+    categories = await homeApiController.categories(context: Get.context!);
+  }
 }
