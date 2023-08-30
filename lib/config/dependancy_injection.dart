46b0d5082c61f09db0ea8883405f63c4f99e1c7c
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-30 16:00:57 +0300

    inject all api config in get it

--- lib/config/dependancy_injection.dart
+++ lib/config/dependancy_injection.dart
@@ -1,14 +1,42 @@
+import 'package:avatar_course2_5_shop/core/internet_checker/internet_checker.dart';
+import 'package:avatar_course2_5_shop/core/network/api/app_api.dart';
+import 'package:avatar_course2_5_shop/core/network/api/dio_factory.dart';
 import 'package:avatar_course2_5_shop/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart';
 import 'package:avatar_course2_5_shop/features/home/presentation/controller/home_controller.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import '../features/auth/presentation/controller/auth_controller.dart';
 import '../features/splash/presentation/controller/splash_controller.dart';
+
+final instance = GetIt.instance;
 
 initModule() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppSettingsSharedPreferences().initPreferences();
+
+  if (!GetIt.I.isRegistered<NetworkInfo>()) {
+    instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImplementation(
+        InternetConnectionCheckerPlus(),
+      ),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<DioFactory>()) {
+    instance.registerLazySingleton<DioFactory>(
+      () => DioFactory(),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<AppApi>()) {
+    Dio dio = await instance<DioFactory>().getDio();
+    instance.registerLazySingleton<AppApi>(
+      () => AppApi(dio),
+    );
+  }
 }
 
 initSplash() {
