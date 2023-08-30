343b6c31b7fe220dba9998c8c97a0012ada39c0e
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-08-30 17:31:08 +0300

    create request

--- lib/core/network/api/app_api.dart
+++ lib/core/network/api/app_api.dart
@@ -2,9 +2,14 @@ import 'package:avatar_course2_5_shop/core/constants.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
+import '../../../features/product_details/data/response/product_details_response.dart';
+
 part 'app_api.g.dart';
 
 @RestApi(baseUrl: ApiRequest.baseUrl)
 abstract class AppApi {
   factory AppApi(Dio dio, {String baseUrl}) = _AppApi;
+  
+  @GET(ApiRequest.productDetails)
+  Future<ProductDetailsResponse> productDetails();
 }
