b2075d933aa4fde251462a42cb546a5faee55099
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-09-06 16:42:43 +0300

    create route

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -2,6 +2,7 @@ import 'package:avatar_course2_5_shop/config/dependancy_injection.dart';
 import 'package:avatar_course2_5_shop/core/resources/manager_strings.dart';
 import 'package:avatar_course2_5_shop/features/auth/presentation/view/register_view.dart';
 import 'package:avatar_course2_5_shop/features/product_details/presentation/view/item_details_view.dart';
+import 'package:avatar_course2_5_shop/features/settings/presentation/view/screen/locale_view.dart';
 import 'package:flutter/material.dart';
 import '../features/auth/presentation/view/authentication_view.dart';
 import '../features/auth/presentation/view/login_view.dart';
@@ -17,6 +18,7 @@ class Routes {
   static const String registerView = '/registerView';
   static const String homeView = '/HomeView';
   static const String itemDetails = '/itemDetails';
+  static const String language = '/language';
 }
 
 class RouteGenerator {
@@ -40,6 +42,9 @@ class RouteGenerator {
       case Routes.itemDetails:
         initProductDetails();
         return MaterialPageRoute(builder: (_) => const ItemDetails());
+        case Routes.language:
+          initLocale();
+        return MaterialPageRoute(builder: (_) => const LocaleView());
       default:
         return unDefineRoute();
     }
